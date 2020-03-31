6c6bef6815ea124c27a0dc9e138dc6d870b7dc75
Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
2020-03-31 18:02:25 +0800

    cherry pick #134 to release-1.1
    Signed-off-by: sre-bot <sre-bot@pingcap.com>

--- scripts/generate_pdf.sh
+++ scripts/generate_pdf.sh
@@ -18,11 +18,20 @@ lang=(en zh)
 
 for d in "${lang[@]}"
 do
+if [ $d = "en" ]; then
+    docs_title=" TiDB in Kubernetes Documentation"
+else
+    docs_title=" TiDB in Kubernetes 用户文档"
+fi
 pandoc -N --toc --smart --latex-engine=xelatex \
 --template=templates/template.tex \
 --columns=80 \
 --listings \
+<<<<<<< HEAD
 -V title="TiDB Documentation" \
+=======
+-V title="$docs_title" \
+>>>>>>> 2bae619... update PDF title (#134)
 -V author="PingCAP Inc." \
 -V date="${_version_tag}" \
 -V CJKmainfont="${MAINFONT}" \
