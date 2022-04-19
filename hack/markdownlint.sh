d73b5b9631596df4066eede730c8bcdead44e859
Oreoxmt <aolinz@outlook.com>
2022-04-19 13:11:17 +0800

    - add markdownlint.sh - avto-dev/markdown-lint

---
+++ hack/markdownlint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# This script runs markdownlint according to ./.markdownlint.yaml.
+#
+# It replaces avto-dev/markdown-lint to avoid multiple setups of node.js environment.
+#
+# See https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally if you meet permission problems when executing npm install.
+
+set -euo pipefail
+
+ROOT=$(unset CDPATH && cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)
+cd $ROOT
+
+npm install markdownlint-cli@0.26.0
+echo "info: running markdownlint under $ROOT directory..."
+npx markdownlint -c ./.markdownlint.yaml -i node_modules .
